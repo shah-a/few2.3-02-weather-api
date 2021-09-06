167c33acb592fa40149d6e8422c98833d2de669d
Ali Shah <s.ashah@outlook.com>
2021-09-05 21:34:14 -0400

    [Cleanup]

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-4xl text-center mt-10 text-blue-500">Salaam, world :)</h1>
+    </div>
+  );
+}
+
+export default App;
