4cad9244522882ee2ee8e5defe877b8ce492099a
Ali Shah <s.ashah@outlook.com>
2021-09-05 22:07:33 -0400

    [Added] footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 function App() {
   return (
-    <div className="App">
+    <div className="App flex flex-col">
       <h1 className="text-4xl text-center mt-10 text-blue-500">Salaam, world :)</h1>
+      <footer className="mt-auto p-14 text-white bg-blue-500">
+        <p>&copy; Ali Shah, {new Date().getUTCFullYear()}</p>
+      </footer>
     </div>
   );
 }
