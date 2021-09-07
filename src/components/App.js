9a0000243a5aee8e5c7d4bc441d404072e4048ce
Ali Shah <s.ashah@outlook.com>
2021-09-07 05:09:22 -0400

    [Added] tailwind playground card example for ref.
    NOTES:
    - https://play.tailwindcss.com/

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import Weather from './Weather/Weather';
 import Footer from './Footer/Footer';
+import Tailwind from './Tailwind';
 
 const App = () => {
   return (
     <div className="App min-h-screen flex flex-col">
       <Weather />
+      {/* <Tailwind /> */}
       <Footer />
     </div>
   );
--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,11 +1,23 @@
 import { useState } from 'react';
 
 const Weather = () => {
+  const [code, setCode] = useState('');
+
   return (
-    <div className="Weather">
-      <h1 className="text-blue-500">
+    <div className="Weather h-full m-auto">
+      <h1 className="text-4xl mt-5 text-blue-500">
         Weather component
       </h1>
+      <div className="input">
+        <input
+          type="text"
+          placeholder="ZIP/Postal Code"
+          className="border-2 rounded-l-md"
+          value={code}
+          onChange={(e) => setCode(e.target.value)}
+        />
+        <button>Submit</button>
+      </div>
     </div>
   );
 };
