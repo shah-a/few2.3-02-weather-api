4cec379afd1ea3357f40cade99e6dab2cde03183
Ali Shah <s.ashah@outlook.com>
2021-09-09 04:58:07 -0400

    [Added] select dropdown and radio buttons

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -2,20 +2,45 @@ import { useState } from 'react';
 
 const Weather = () => {
   const [code, setCode] = useState('');
+  const [unit, setUnit] = useState('');
+
+  const units = ['metric', 'imperial', 'standard'];
+  const accentColor = { accentColor: '' };
+
+  const radioButtons = units.map((unit) => {
+    return (
+      <label className="mt-1">
+        <input style={accentColor} type="radio" name="unit" className="mr-1" />
+        {`${unit[0].toUpperCase()}${unit.slice(1)}`}
+      </label>);
+  });
 
   return (
     <div className="Weather m-auto">
-      <form>
-        <h1 className="text-5xl">Weather in:</h1>
+      <form className="flex flex-col">
+        <h1 className="text-5xl mb-5">Weather in:</h1>
         <h1 className="text-5xl mb-10 w-full text-blue-500 font-bold">{code}</h1>
-        <input
-          type="text"
-          placeholder="ZIP/Postal Code"
-          className="p-2 border border-blue-500 rounded-l"
-          value={code}
-          onChange={(e) => setCode(e.target.value)}
-        />
-        <button className="p-2 bg-blue-500 text-white border border-blue-500 rounded-r">Submit</button>
+        <div className="mb-5">
+          <input
+            type="text"
+            placeholder="ZIP/Postal Code"
+            className="p-2 border border-blue-500 rounded-l"
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
+          />
+          <button className="p-2 bg-blue-500 text-white border border-blue-500 rounded-r">Submit</button>
+        </div>
+        <select
+          style={accentColor}
+          className="mb-3 p-2 border border-blue-500 rounded mr-auto"
+          onChange={(e) => setUnit(e.target.value)}
+        >
+          <option value="metric">Celsius</option>
+          <option value="imperial">Fahrenheit</option>
+          <option value="standard">Kelvin</option>
+        </select>
+
+        {radioButtons}
       </form>
     </div>
   );
