12c02605596ab91acd9f5fae47682bb4097841c2
Ali Shah <s.ashah@outlook.com>
2021-09-09 18:11:25 -0400

    [Added] data fetch logic

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,30 +1,47 @@
 import { useState } from 'react';
 import RadioButton from '../RadioButton/RadioButton';
+import fetchWeather from '../../utils/fetchWeather';
 
 const Weather = () => {
   const [code, setCode] = useState('');
-  const [unit, setUnit] = useState('metric');
+  const [units, setUnits] = useState('metric');
+  const [data, setData] = useState(null);
+
+  console.log(data);
 
   return (
     <div className="Weather m-auto">
-      <form className="flex flex-col">
+      <form
+        className="flex flex-col"
+        onSubmit={(e) => {
+          e.preventDefault();
+          fetchWeather(code, units, setData);
+        }}
+      >
         <h1 className="text-5xl mb-5">Weather in:</h1>
         <h1 className="text-5xl mb-10 w-full text-blue-500 font-bold">{code}</h1>
-        <p className="mb-5">Units: {`${unit[0].toUpperCase()}${unit.slice(1)}`}</p>
+        <p>{data && data.weather[0].description}</p>
+        <p className="mb-5">Selected Units: {`${units[0].toUpperCase()}${units.slice(1)}`}</p>
         <div className="mb-5">
           <input
             type="text"
             placeholder="ZIP/Postal Code"
+            pattern="^([A-Za-z]\d[A-Za-z])|(\d{5})$"
             className="p-2 border border-blue-500 rounded-l"
             value={code}
             onChange={(e) => setCode(e.target.value)}
           />
-          <button className="p-2 bg-blue-500 text-white border border-blue-500 rounded-r">Submit</button>
+          <button
+            type="submit"
+            className="p-2 bg-blue-500 text-white border border-blue-500 rounded-r"
+          >
+            Submit
+          </button>
         </div>
         <select
-          value={unit}
+          value={units}
           className="mb-3 p-2 border border-blue-500 rounded mr-auto"
-          onChange={(e) => setUnit(e.target.value)}
+          onChange={(e) => setUnits(e.target.value)}
         >
           <option value="metric">Celsius</option>
           <option value="imperial">Fahrenheit</option>
@@ -33,9 +50,10 @@ const Weather = () => {
 
         {['metric', 'imperial', 'standard'].map((val) =>
           <RadioButton
-            name="unit"
-            checked={unit === val}
-            onChange={() => setUnit(val)}
+            key={`key-${val}`}
+            name="units"
+            checked={units === val}
+            onChange={() => setUnits(val)}
             label={`${val[0].toUpperCase()}${val.slice(1)}`}
           />
         )}
---
+++ src/utils/fetchWeather.js
@@ -0,0 +1,15 @@
+const fetchWeather = async (code, units, setData) => {
+  const apiKey = process.env.REACT_APP_API_KEY;
+
+  code = new RegExp(/([a-z]\d[a-z])/i).test(code) ? `${code},ca` : code;
+
+  const urlBase = process.env.REACT_APP_URL_BASE;
+  const urlQuery = `?zip=${code}&appid=${apiKey}&${units}`
+
+  const res = await fetch(urlBase + urlQuery);
+  const data = await res.json();
+
+  setData(data);
+}
+
+export default fetchWeather;
