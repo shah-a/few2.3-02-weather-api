e4096cafb81ed9ad65e24ea4a660c927186aaa53
Ali Shah <s.ashah@outlook.com>
2021-09-09 00:48:58 -0400

    [Updated] input styles

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -4,20 +4,19 @@ const Weather = () => {
   const [code, setCode] = useState('');
 
   return (
-    <div className="Weather h-full m-auto">
-      <h1 className="text-4xl mt-5 text-blue-500">
-        Weather component
-      </h1>
-      <div className="input">
+    <div className="Weather m-auto">
+      <form>
+        <h1 className="text-5xl">Weather in:</h1>
+        <h1 className="text-5xl mb-10 w-full text-blue-500 font-bold">{code}</h1>
         <input
           type="text"
           placeholder="ZIP/Postal Code"
-          className="border-2 rounded-l-md"
+          className="p-2 border border-blue-500 rounded-l"
           value={code}
           onChange={(e) => setCode(e.target.value)}
         />
-        <button>Submit</button>
-      </div>
+        <button className="p-2 bg-blue-500 text-white border border-blue-500 rounded-r">Submit</button>
+      </form>
     </div>
   );
 };
