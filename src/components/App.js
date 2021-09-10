c8c37dc9b245cf7a7c00904493d58e65c32f414e
Ali Shah <s.ashah@outlook.com>
2021-09-10 09:12:42 -0400

    [Update] increased margin for gradient shadow

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,8 @@ const App = () => {
     <div className="App min-h-screen flex flex-col bg-gray-100">
       <div className="my-auto sm:mx-auto">
         <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-          <div className="absolute inset-0 bg-gradient shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-          <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <div className="absolute inset-x-0 inset-y-10 sm:inset-10 bg-gradient shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+          <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20 my-10 sm:m-10">
             <Weather />
           </div>
         </div>
