96d016f426f39aaa989bad161604b2fc780bb6d9
Ali Shah <s.ashah@outlook.com>
2021-09-10 04:08:16 -0400

    [Updated] styles

--- src/components/Weather/CodeInput/CodeInput.js
+++ src/components/Weather/CodeInput/CodeInput.js
@@ -3,10 +3,12 @@ const CodeInput = (props) => {
 
   return (
     <div className="CodeInput mb-3">
+      <small className="block mb-2">(For Canadian postal codes, first 3 characters only)</small>
       <input
         type="text"
         placeholder="ZIP/Postal Code"
         pattern="^([A-Za-z]\d[A-Za-z])|(\d{5})$"
+        maxLength="5"
         className="p-2 border border-blue-500 rounded-l"
         value={code}
         onChange={(e) => setCode(e.target.value)}
--- src/components/Weather/UnitsRadio/UnitsRadio.js
+++ src/components/Weather/UnitsRadio/UnitsRadio.js
@@ -2,7 +2,7 @@ const UnitsRadio = (props) => {
   const { label, name, checked, onChange } = props;
 
   return (
-    <label className="mb-1">
+    <label className="block mb-1">
       <input
         type="radio"
         className="mr-1"
