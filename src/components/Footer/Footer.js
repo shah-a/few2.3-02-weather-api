31af3ccf68274ed78eb3ee1d60e19899f424f368
Ali Shah <s.ashah@outlook.com>
2021-09-10 09:30:58 -0400

    [Update] brought footer back for smaller screens

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="Footer mt-auto bg-gradient-reverse hidden sm:block">
+    <footer className="Footer mt-auto bg-gradient-reverse">
       <a href="https://github.com/shah-a/few2.3-02-weather-api" target="_blank" rel="noreferrer">
         <i className="fab fa-github-alt fa-3x p-10 text-teal-900 hover:text-teal-700"></i>
       </a>
