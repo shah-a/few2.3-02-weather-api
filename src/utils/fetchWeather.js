9a58fa3ab573afe431524f8a35589cddbd4287e5
Ali Shah <s.ashah@outlook.com>
2021-09-22 17:36:58 -0400

    [Update] modified API call handling
    NOTES:
    - Needed to adjust how the API call is made so different query
      strings can be provided from the frontend

--- src/utils/fetchWeather.js
+++ src/utils/fetchWeather.js
@@ -1,15 +1,20 @@
 const fetchWeather = async (code, units, setData) => {
   code = new RegExp(/([a-z]\d[a-z])/i).test(code) ? `${code},ca` : code;
 
-  const urlBase = process.env.REACT_APP_URL_BASE;
-  const urlQuery = `?code=${code}&units=${units}`;
+  const urlBase = 'https://api.openweathermap.org/data/2.5/weather';
+  const urlQuery = `?zip=${code}&units=${units}`;
 
-  let data = await fetch(urlBase + urlQuery);
+  const fetchUrl = `${process.env.REACT_APP_BACKEND_URL}/weather`;
+  const method = 'POST';
+  const headers = { 'Content-Type': 'application/json' };
+  const body = JSON.stringify({ urlBase, urlQuery });
+
+  let data = await fetch(fetchUrl, { method, headers, body });
   data = await data.json();
 
   // If the status code begins with anything other than 2, then
   // `setData(null)`. Otherwise, `setData(data)`.
-  data.cod.toString()[0] !== "2" ? setData(null) : setData({
+  data.cod.toString()[0] !== '2' ? setData(null) : setData({
     temp: data.main.temp,
     feelsLike: data.main.feels_like,
     description: data.weather[0].description
