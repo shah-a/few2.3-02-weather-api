c732125362e09308d6dd8af3545861fa885dec1a
Ali Shah <s.ashah@outlook.com>
2021-09-23 02:35:53 -0400

    [Added] functionality to get weather by geo coords

--- src/utils/fetchWeather.js
+++ src/utils/fetchWeather.js
@@ -22,7 +22,37 @@ const fetchWeather = async (code, units, setData) => {
 }
 
 const fetchWeatherGeo = async (units, setData) => {
-  await new Promise((res) => setTimeout(res, 1500));
+  const geoPromise = new Promise((res, rej) => {
+    navigator.geolocation.getCurrentPosition(res, rej, {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    })
+  });
+
+  const pos = await geoPromise.catch(() => alert('Error! Try checking your location permissions.'));
+
+  if (!pos) return setData(null);
+
+  const { latitude, longitude } = pos.coords;
+  const urlBase = 'https://api.openweathermap.org/data/2.5/weather';
+  const urlQuery = `?lat=${latitude}&lon=${longitude}&units=${units}`;
+
+  const fetchUrl = `${process.env.REACT_APP_BACKEND_URL}/weather`;
+  const method = 'POST';
+  const headers = { 'Content-Type': 'application/json' };
+  const body = JSON.stringify({ urlBase, urlQuery });
+
+  let data = await fetch(fetchUrl, { method, headers, body });
+  data = await data.json();
+
+  // If the status code begins with anything other than 2, then
+  // `setData(null)`. Otherwise, `setData(data)`.
+  data.cod.toString()[0] !== '2' ? setData(null) : setData({
+    temp: data.main.temp,
+    feelsLike: data.main.feels_like,
+    description: data.weather[0].description
+  });
 };
 
 export { fetchWeather, fetchWeatherGeo };
