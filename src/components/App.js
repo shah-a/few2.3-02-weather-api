00b9425cea2ab91ebe55ad32e003c6befe21e000
Ali Shah <s.ashah@outlook.com>
2021-10-01 05:52:05 -0400

    [Fixed] button display issue on 1080p monitor

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className="App min-h-screen flex flex-col bg-gray-100">
       <div className="my-auto sm:mx-auto">
-        <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="relative py-3 sm:max-w-2xl sm:mx-auto">
           <div className="absolute inset-x-0 inset-y-10 sm:inset-10 bg-gradient shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
           <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20 my-10 sm:m-10">
             <Weather />
